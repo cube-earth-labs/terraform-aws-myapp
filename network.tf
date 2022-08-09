ab154fde004e1eeaf42cab46c2e91a00c81d4b70
Troy Fluegge <troy@hashicorp.com>
2022-08-09 11:52:48 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -30,8 +30,8 @@ resource "aws_security_group" "myapp" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
