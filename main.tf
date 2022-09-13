068d5767ec5f3f980b1c2565e32da4ccc3a2aa4b
Troy Fluegge <troy@hashicorp.com>
2022-09-13 08:11:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-/* Non HCP Packer AMI lookup
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# /* Non HCP Packer AMI lookup
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
+#   filter {
+#     name = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
-*/
+#   owners = ["099720109477"] # Canonical
+# }
+# */
 
 data "hcp_packer_iteration" "myapp" {
   bucket_name = "hcp-packer-myapp"
@@ -29,8 +29,8 @@ data "hcp_packer_image" "myapp" {
 }
 
 resource "aws_instance" "myapp" {
-  #ami                         = data.hcp_packer_image.myapp.cloud_image_id
-  ami                         = "ami-0568773882d492fc8"
+  ami                         = data.hcp_packer_image.myapp.cloud_image_id
+  #ami                         = "ami-0568773882d492fc8"
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.myapp.key_name
   associate_public_ip_address = true
