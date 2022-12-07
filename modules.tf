5de34bbd0fb16f6c9a9a734e1218ce9f8f6c38ba
Troy Fluegge <troy@hashicorp.com>
2022-12-07 09:58:17 -0600

    Module Testing

--- modules.tf
+++ modules.tf
@@ -5,27 +5,27 @@ module "s3_bucket" {
   acl    = "private"
 }
 
-module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.16.2"
-  vpc_id = module.vpc.vpc_id
-  name        = "test-sg"
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-}
+# module "security-group" {
+#   source  = "terraform-aws-modules/security-group/aws"
+#   version = "4.16.2"
+#   vpc_id = module.vpc.vpc_id
+#   name        = "test-sg"
+#   ingress_cidr_blocks      = ["10.10.0.0/16"]
+#   ingress_rules            = ["https-443-tcp"]
+#   ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 8080
+#       to_port     = 8090
+#       protocol    = "tcp"
+#       description = "User-service ports"
+#       cidr_blocks = "10.10.0.0/16"
+#     },
+#     {
+#       rule        = "postgresql-tcp"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#   ]
+# }
 
 # module "vpc" {
 #   source  = "terraform-aws-modules/vpc/aws"
