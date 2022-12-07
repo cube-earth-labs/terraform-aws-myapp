7b5c10f2bbda505fa860a3fbeb9c2267b0eed896
Troy Fluegge <troy@hashicorp.com>
2022-12-07 09:57:18 -0600

    Module Testing

--- modules.tf
+++ modules.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "3.6.0"
-  bucket = "troyf-myapp-bucket"
+  bucket = "${var.prefix}-myapp-bucket-${var.environment}"
   acl    = "private"
 }
 
@@ -27,13 +27,13 @@ module "security-group" {
   ]
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
-  name = "test-vpc"
-  cidr = "10.0.0.0/16"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "3.18.1"
+#   name = "test-vpc"
+#   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
+#   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+# }
