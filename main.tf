b27db77b40c65a9c71ce54debf34ad29a5c0d2dd
Troy Fluegge <troy@hashicorp.com>
2023-04-12 13:55:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,21 +16,21 @@
 # }
 # */
 
-data "hcp_packer_iteration" "myapp" {
-  bucket_name = "hcp-packer-myapp"
-  channel     = var.environment
-}
+# data "hcp_packer_iteration" "myapp" {
+#   bucket_name = "hcp-packer-myapp"
+#   channel     = var.environment
+# }
 
-data "hcp_packer_image" "myapp" {
-  bucket_name    = data.hcp_packer_iteration.myapp.bucket_name
-  cloud_provider = "aws"
-  iteration_id   = var.iteration_id == null ? data.hcp_packer_iteration.myapp.ulid : var.iteration_id
-  region         = var.region
-}
+# data "hcp_packer_image" "myapp" {
+#   bucket_name    = data.hcp_packer_iteration.myapp.bucket_name
+#   cloud_provider = "aws"
+#   iteration_id   = var.iteration_id == null ? data.hcp_packer_iteration.myapp.ulid : var.iteration_id
+#   region         = var.region
+# }
 
 resource "aws_instance" "myapp" {
-  ami                         = data.hcp_packer_image.myapp.cloud_image_id
-  #ami                         = "ami-0568773882d492fc8"
+  #ami                         = data.hcp_packer_image.myapp.cloud_image_id
+  ami                         = "ami-0568773882d492fc8"
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.myapp.key_name
   associate_public_ip_address = true
@@ -39,9 +39,9 @@ resource "aws_instance" "myapp" {
   user_data = file("${path.module}/scripts/userdata-server.sh")
   tags = {
     Name = "${var.prefix}-myapp-instance"
-    HCP-Image-Channel = data.hcp_packer_image.myapp.channel
-    HCP-Iteration-ID = data.hcp_packer_iteration.myapp.ulid
-    HCP-Image-Version = data.hcp_packer_iteration.myapp.incremental_version
-    HCP-Image-Creation = data.hcp_packer_iteration.myapp.created_at
+    #HCP-Image-Channel = data.hcp_packer_image.myapp.channel
+    #HCP-Iteration-ID = data.hcp_packer_iteration.myapp.ulid
+    #HCP-Image-Version = data.hcp_packer_iteration.myapp.incremental_version
+    #HCP-Image-Creation = data.hcp_packer_iteration.myapp.created_at
   }
 }
