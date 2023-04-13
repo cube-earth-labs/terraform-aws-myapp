b7ea0fa78ce58bcdc6ae318b87f51d3d4dce0147
Troy Fluegge <troy@hashicorp.com>
2023-04-13 10:14:33 -0500

    Added lifecycle condition

--- main.tf
+++ main.tf
@@ -44,5 +44,12 @@ resource "aws_instance" "myapp" {
     HCP-Iteration-ID = data.hcp_packer_iteration.myapp.ulid
     HCP-Image-Version = data.hcp_packer_iteration.myapp.incremental_version
     HCP-Image-Creation = data.hcp_packer_iteration.myapp.created_at
+
+  lifecycle {
+    postcondition {
+      condition     = self.ami == data.hcp_packer_image.myapp.cloud_image_id
+      error_message = "Must use the latest available AMI,
+        ${data.hcp_packer_image.myapp.cloud_image_id}."
+    }
   }
 }
