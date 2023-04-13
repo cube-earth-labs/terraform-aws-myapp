11814c06d98ca6f99211df0a45a5e7bec31121d5
Troy Fluegge <troy@hashicorp.com>
2023-04-13 10:03:17 -0500

    Updated image source

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
-# /* Non HCP Packer AMI lookup
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# Non HCP Packer AMI lookup
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
+#   filter {
+#     name = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
+#   owners = ["099720109477"] # Canonical
+# }
+#
+
+data "hcp_packer_iteration" "myapp" {
+  bucket_name = "hcp-packer-myapp"
+  channel     = var.environment
 }
-# */
 
-# data "hcp_packer_iteration" "myapp" {
-#   bucket_name = "hcp-packer-myapp"
-#   channel     = var.environment
-# }
-
-# data "hcp_packer_image" "myapp" {
-#   bucket_name    = data.hcp_packer_iteration.myapp.bucket_name
-#   cloud_provider = "aws"
-#   iteration_id   = var.iteration_id == null ? data.hcp_packer_iteration.myapp.ulid : var.iteration_id
-#   region         = var.region
-# }
+data "hcp_packer_image" "myapp" {
+  bucket_name    = data.hcp_packer_iteration.myapp.bucket_name
+  cloud_provider = "aws"
+  iteration_id   = var.iteration_id == null ? data.hcp_packer_iteration.myapp.ulid : var.iteration_id
+  region         = var.region
+}
 
 resource "aws_instance" "myapp" {
-  #ami                         = data.hcp_packer_image.myapp.cloud_image_id
-  ami                         = data.aws_ami.ubuntu.id
-  #ami                         = "ami-0568773882d492fc8"
+  ami                         = data.hcp_packer_image.myapp.cloud_image_id   # Retrieving from HCP Packer registry
+  #ami                         = data.aws_ami.ubuntu.id   # Retrieving AMI ID from AWS data filter
+  #ami                         = "ami-0568773882d492fc8"  # Direct AMI ID assignment
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.myapp.key_name
   associate_public_ip_address = true
@@ -40,9 +40,9 @@ resource "aws_instance" "myapp" {
   user_data = file("${path.module}/scripts/userdata-server.sh")
   tags = {
     Name = "${var.prefix}-myapp-instance"
-    #HCP-Image-Channel = data.hcp_packer_image.myapp.channel
-    #HCP-Iteration-ID = data.hcp_packer_iteration.myapp.ulid
-    #HCP-Image-Version = data.hcp_packer_iteration.myapp.incremental_version
-    #HCP-Image-Creation = data.hcp_packer_iteration.myapp.created_at
+    HCP-Image-Channel = data.hcp_packer_image.myapp.channel
+    HCP-Iteration-ID = data.hcp_packer_iteration.myapp.ulid
+    HCP-Image-Version = data.hcp_packer_iteration.myapp.incremental_version
+    HCP-Image-Creation = data.hcp_packer_iteration.myapp.created_at
   }
 }
