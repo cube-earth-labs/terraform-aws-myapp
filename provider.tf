20a965922108e053c3d3edd346bed737c6a9fee9
Eric Reeves <eric@alluvium.com>
2023-07-18 11:42:52 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -26,7 +26,7 @@ provider "aws" {
       environment = var.environment
       application = "MyApp"
       owner       = "Eric"
-      costcenter  = "1234"
+      costcenter  = "12345"
     }
   }
 }
